03f843feb78882f118a8fe13729516a762b4bc25
Akseli Palén <akseli.palen@gmail.com>
2018-01-11 20:26:16 +0200

    Improved Grid with steps and phases

--- test/Grid.test.js
+++ test/Grid.test.js
@@ -7,18 +7,21 @@ module.exports = function (test) {
 
   test('constructor when no parameters', function (t) {
     var gr = new Grid()
-    t.equal(gr.mode.scale, 0, 'default scale step')
+    t.equal(gr.mode.scaleStep, 0, 'default scale step')
     t.end()
   })
 
   test('#snap translate', function (t) {
     var tr = Transform.IDENTITY.translateBy(15, 5)
     var gr = new Grid({
-      translate: 10
+      xStep: 10,
+      yStep: 10,
+      xPhase: 2,
+      yPhase: 0
     })
     var snapped = gr.snap(tr)
 
-    t.equal(snapped.tx, 20, 'tx')
+    t.equal(snapped.tx, 12, 'tx')
     t.equal(snapped.ty, 10, 'ty')
 
     t.end()
@@ -27,11 +30,11 @@ module.exports = function (test) {
   test('#snap rotate', function (t) {
     var tr = Transform.IDENTITY.rotateBy(Math.PI * 1.8)
     var gr = new Grid({
-      rotate: Math.PI
+      rotateStep: Math.PI
     })
     var snapped = gr.snap(tr)
 
-    t.equal(snapped.getRotation(), 0, 'rotation')
+    t.equal(snapped.getRotation(), 0, 'full turn')
 
     t.end()
   })
