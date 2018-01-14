0f271feb4583ec753367b687d00959dc157eeb66
Akseli Palén <akseli.palen@gmail.com>
2018-01-14 22:21:34 +0200

    v4.0.0-alpha.13 improve grid, still buggy

--- test/Grid.test.js
+++ test/Grid.test.js
@@ -13,45 +13,134 @@ module.exports = function (test) {
 
   test('#snap translate', function (t) {
     var tr = Transform.IDENTITY.translateBy(15, 5)
-    var gr = new Grid({
+    var grid = new Grid({
       xStep: 10,
       yStep: 10,
       xPhase: 2,
       yPhase: 0
     })
-    var snapped = gr.snap(tr)
+    var pivot = new taaspace.Vector(0, 0)
+    var snapped = grid.snap(pivot, tr)
 
     t.equal(snapped.tx, 12, 'tx')
     t.equal(snapped.ty, 10, 'ty')
+
+    var pivot2 = new taaspace.Vector(8, 8)
+    // tr maps pivot to (23, 13)
+    // nearest point on grid is (22, 10)
+    // Therefore a translation of (-1, -3) is required to snap.
+    var snapped2 = grid.snap(pivot2, tr)
+
+    t.equal(snapped2.tx, 14, 'tx pivot 8 8')
+    t.equal(snapped2.ty, 2, 'ty pivot 8 8')
+
+    t.end()
+  })
+
+  test('#snap with pivot', function (t) {
+    // Remain same with pivot 0 0
+    var gr = new Grid({
+      rotateStep: Math.PI / 4,
+      rotatePhase: 0
+    })
+    var pivot = new taaspace.Vector(0, 0)
+    var deg45 = Transform.IDENTITY.rotateBy(Math.PI / 4)
+    var snapped = gr.snap(pivot, deg45)
+    t.ok(snapped.equals(deg45), 'stay same, pivot 0 0')
+
+    // Snap to closest
+    var deg36 = Transform.IDENTITY.rotateBy(Math.PI / 5)
+    var snapped2 = gr.snap(pivot, deg36)
+    t.ok(snapped2.almostEqual(deg45), 'back to deg45, pivot 0 0')
+
+    // Remain same with pivot 1 1
+    var pivot2 = new taaspace.Vector(1, 1)
+    var snapped3 = gr.snap(pivot2, deg45)
+    t.ok(snapped3.equals(deg45), 'stay same, pivot 1 1')
+
+    // Snap to closest with pivot 1 1
+    var snapped4 = gr.snap(pivot2, deg36)
+    t.notOk(snapped4.equals(deg45), 'translation should change')
+    t.equal(snapped4.getRotation(), Math.PI / 4, 'rotation snapped')
 
     t.end()
   })
 
   test('#snap scale', function (t) {
     var tr = Transform.IDENTITY.scaleBy(3.2)
-    var gr = new Grid({
+    var grid = new Grid({
       scaleStep: 1,
       scalePhase: 0.5
     })
-    var snapped = gr.snap(tr)
+    var pivot = new taaspace.Vector(0, 0)
+    var snapped = grid.snap(pivot, tr)
 
     t.equal(snapped.getScale(), 3.5, 'snapped scale')
     t.equal(snapped.getRotation(), 0, 'zero rotation')
-    t.equal(snapped.tx, 0, 'zero tx')
+    t.ok(pivot.transform(tr).equals(pivot.transform(snapped)),
+      'pivot stay still')
+
+    // With non-origin pivot
+    var pivot2 = new taaspace.Vector(1, 1)
+    var snapped2 = grid.snap(pivot2, tr)
+
+    t.ok(pivot2.transform(tr).equals(pivot2.transform(snapped2)),
+      'pivot2 stay still')
+    t.notOk(tr.equals(snapped2), 'transformations not same')
 
     t.end()
   })
 
   test('#snap rotate', function (t) {
-    var tr = Transform.IDENTITY.rotateBy(Math.PI * 1.8)
-    var gr = new Grid({
+    var grid = new Grid({
       rotateStep: Math.PI
     })
-    var snapped = gr.snap(tr)
+    var pivot = new taaspace.Vector(0, 0)
+    var tr = Transform.IDENTITY.rotateBy(Math.PI * 1.8)
+    var snapped = grid.snap(pivot, tr)
 
-    t.equal(snapped.getRotation(), 0, 'full turn')
+    t.ok(snapped.getRotation() < Transform.EPSILON, 'full turn')
 
     t.end()
   })
 
+  test('#snap xRotation', function (t) {
+    // Let us build a grid where allowed x values go at 45 degree angle.
+    //
+    //     | /         /         /
+    //     |/         /         /
+    //   --+-->      /         /
+    //    /|        /         /
+    //   / |       /         /
+    //  /  V      /         /
+    //
+    var grid = new Grid({
+      xStep: 10,
+      xRotation: Math.PI / 4
+    })
+    var pivot = new taaspace.Vector(0, 0)
+    var tr = Transform.IDENTITY.translateBy(4, 0)
+
+    var snapped = grid.snap(pivot, tr)
+
+    var tr2 = Transform.IDENTITY.translateBy(2, -2)
+    t.ok(snapped.almostEqual(tr2), 'snapped to nearest')
+
+    t.end()
+  })
+
+  test('#transform', function (t) {
+    var gr1 = new Grid({
+      xStep: 10
+    })
+
+    var gr2 = new Grid({
+      xStep: 20
+    })
+
+    var gr1X2 = gr1.transform(Transform.X2)
+
+    t.ok(gr1X2.equal(gr2), 'grid scaled')
+    t.end()
+  })
 }
--- test/InvariantGrid.test.js
+++ test/InvariantGrid.test.js
@@ -14,7 +14,7 @@ module.exports = function (test) {
     }), space)
 
     // Position the first pixel with a grid and second with a translation.
-    px1.snap(igrid)
+    px1.snap(space.at(0, 0), igrid)
     px2.translate(space.at(0, 0), space.at(2, 0))
 
     // Positions should match
@@ -22,6 +22,80 @@ module.exports = function (test) {
     var gt2 = px2.getGlobalTransform()
 
     t.ok(gt1.equals(gt2), 'positions should be same')
+    t.end()
+  })
+
+  test('#snap with pivot', function (t) {
+    var space = new taaspace.Space()
+    var px1 = new taaspace.SpacePixel(space)
+    var px2 = new taaspace.SpacePixel(space)
+
+    // Move both away from same coordinate system with space.
+    // This allows us to see problems with correct use of planes.
+    px1.translate(space.at(0, 0), space.at(10, 0))
+    px2.translate(space.at(0, 0), space.at(10, 0))
+
+    var igrid = new taaspace.InvariantGrid(new taaspace.Grid({
+      xStep: 10,
+      rotateStep: Math.PI / 2,
+      rotatePhase: Math.PI / 4
+    }), space)
+
+    // Position the first pixel with a grid and second with a translation.
+    px1.snap(px1.at(2, 2), igrid)
+    px2.translate(px2.at(2, 2), space.at(10, 2))
+    px2.rotate(px2.at(2, 2), Math.PI / 4)
+
+    // Positions should match
+    var gt1 = px1.getGlobalTransform()
+    var gt2 = px2.getGlobalTransform()
+
+    console.log(gt1, gt2)
+    t.ok(gt1.almostEqual(gt2), 'positions should be same')
+    t.end()
+  })
+
+  test('#snap through node hierarchy', function (t) {
+    var ggt, sgt
+
+    var space = new taaspace.Space()
+    var g = new taaspace.SpaceGroup(space)
+    var gpx = new taaspace.SpacePixel(g) // Note: on group
+    var spx = new taaspace.SpacePixel(space) // Note: on space
+
+    // Transform both pixels so that px.atNW()
+    // becomes near space.at(-3, -3) with
+    // positive x axis toward space origin.
+
+    g.translate(space.at(0, 0), space.at(-10, -10))
+    g.rotate(g.at(0, 0), Math.PI / 4)
+    gpx.translate(gpx.at(0, 0), gpx.at(10, 0))
+    // Mimic
+    spx.translate(space.at(0, 0), space.at(-10, -10))
+    spx.rotate(spx.at(0, 0), Math.PI / 4)
+    spx.translate(spx.at(0, 0), spx.at(10, 0))
+
+    // Assert positions equal
+    ggt = gpx.getGlobalTransform()
+    sgt = spx.getGlobalTransform()
+    t.ok(ggt.almostEqual(sgt), 'assert positions equal')
+
+    // //console.log(gpx.atNW().toSpace().toArray())
+    // //
+    // var igrid = new taaspace.InvariantGrid({
+    //   xStep: 10
+    // }, space)
+    //
+    // gpx.snap(gpx.at(0, 0), igrid)
+    // // Mimic
+    // spx.translate(spx.at(0, 0), space.at(0, 0))
+    //
+    // // Assert positions still equal
+    // ggt = gpx.getGlobalTransform()
+    // sgt = spx.getGlobalTransform()
+    // //console.log(ggt._tr, sgt._tr)
+    // t.ok(ggt.almostEqual(sgt), 'assert positions equal')
+
     t.end()
   })
 }
