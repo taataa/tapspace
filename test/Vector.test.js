b4079874f9412cbabf687a178f18850f64d61282
Akseli Palén <akseli.palen@gmail.com>
2018-01-14 18:34:40 +0200

    Implement Vector#changeBasis

--- test/Vector.test.js
+++ test/Vector.test.js
@@ -15,8 +15,39 @@ module.exports = function (test) {
     var v = new Vector(1, 1)
     var v2 = v.add(v)
     t.notOk(v.equals(v2), 'result not equal')
+    t.ok(v2.equal(new Vector(2, 2)), 'alias')
     t.equal(v2.x, 2, 'x')
     t.equal(v2.y, 2, 'y')
+
+    t.end()
+  })
+
+  test('#changeBasis simple', function (t) {
+    var v = new Vector(-2, 2)
+    var i = new Vector(0, 1)
+    var j = new Vector(1, 0)
+    var vij = v.changeBasis(i, j)
+    t.ok(vij.equals(new Vector(2, -2)), 'new basis')
+    t.end()
+  })
+
+  test('#changeBasis simple', function (t) {
+    var v = new Vector(-1, 2.5)
+    var i = new Vector(1, 0.5)
+    var j = new Vector(-1, 0.5)
+    var vij = v.changeBasis(i, j)
+    t.ok(vij.equals(new Vector(2, 3)), 'new basis')
+    t.end()
+  })
+
+  test('#changeBasis dependent basis', function (t) {
+    var v = new Vector(1, 3)
+    var i = new Vector(0, 1)
+    var j = new Vector(0, 2)
+
+    t.throws(function () {
+      v.changeBasis(i, j)
+    }, /independent/, 'gives error')
 
     t.end()
   })
