a0cdd8b5548aa7c02e97f9f31d070685e02a069c
Akseli Palén <akseli.palen@gmail.com>
2018-02-12 11:50:48 +0200

    Fix #66 test for version match

--- test/version.test.js
+++ test/version.test.js
@@ -1,9 +1,15 @@
 var tapspace = require('../index')
+var pjson = require('../package.json')
 var semver = require('semver')
 
 module.exports = function (test) {
   test('correctly formatted', function (t) {
     t.ok(semver.valid(tapspace.version), 'valid semantic version')
+    t.end()
+  })
+
+  test('match package.json', function (t) {
+    t.equal(tapspace.version, pjson.version, 'version match')
     t.end()
   })
 }
