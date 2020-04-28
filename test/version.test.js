33baeb9d1733b0410d56f01e8f530545eba5dbe2
Akseli Palen <akseli.palen@gmail.com>
2020-04-29 01:20:07 +0300

    Remove package-lock version test

--- test/version.test.js
+++ test/version.test.js
@@ -1,6 +1,5 @@
 var tapspace = require('../index')
 var pjson = require('../package.json')
-var plock = require('../package-lock.json')
 var semver = require('semver')
 
 module.exports = function (test) {
@@ -11,11 +10,6 @@ module.exports = function (test) {
 
   test('match package.json', function (t) {
     t.equal(tapspace.version, pjson.version, 'version match')
-    t.end()
-  })
-
-  test('match package-lock.json', function (t) {
-    t.equal(tapspace.version, plock.version, 'lock version match')
     t.end()
   })
 }
