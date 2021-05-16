6956b8fc7c29a3021cb16fdf874434f248811b06
Akseli Palen <akseli.palen@gmail.com>
2021-05-17 00:55:03 +0300

    Init v2 index

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 // API v4
 
-exports.geom = require('./lib/geom')
-exports.Space = require('./lib/Space')
-exports.SpaceGroup = require('./lib/SpaceGroup')
-exports.SpaceHTML = require('./lib/SpaceHTML')
-exports.SpaceImage = require('./lib/SpaceImage')
-exports.SpacePixel = require('./lib/SpacePixel')
-exports.SpaceView = require('./lib/SpaceView')
-exports.Touchable = require('./lib/Touchable')
-exports.Wheelable = require('./lib/Wheelable')
-// Maybe in future:
-// exports.SpacePoint = require('./lib/SpacePoint')
-// exports.SpaceTransform = require('./lib/SpaceTransform')
+const SpaceElement = require('./lib/SpaceElement')
 
+module.exports = SpaceElement.create
+
+exports.geom = require('./lib/geom')
 exports.preload = require('loadimages')
 exports.version = require('./lib/version')
--- v2/index.js
+++
@@ -1,7 +0,0 @@
-exports.geom = require('./lib/geom')
-
-const SpaceElement = require('./lib/SpaceElement')
-
-module.exports = (el) {
-  return new SpaceElement(el)
-}
