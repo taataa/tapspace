d1d535e3257a631958af4be5c0bd1484812498e5
Akseli Palen <akseli.palen@gmail.com>
2021-05-21 03:45:36 +0300

    Init SpaceView class

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // API Tapspace v2
 
 const SpaceElement = require('./lib/SpaceElement')
+const SpaceView = require('./lib/SpaceView')
 
 module.exports = SpaceElement.create
+
+exports.createView = (el) => {
+  console.log('view created')
+}
 
 exports.geom = require('./lib/geom')
 exports.preload = require('loadimages')
