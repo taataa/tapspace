de7b4679845a6b85a95ba790c1f8b91e526b046d
Akseli Palen <akseli.palen@gmail.com>
2022-03-21 09:28:17 +0200

    index api sketching

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 // API Tapspace v2
 
-const SpaceElement = require('./lib/SpaceElement')
-const SpaceView = require('./lib/SpaceView')
+const SpaceElement = require('./lib/Element')
+const SpaceView = require('./lib/View')
 
 module.exports = (el) => {
   return new SpaceElement(el)
 }
 
-exports.createView = (el) => {
+exports.element = (el) => {
+  return new SpaceElement(el)
+}
+
+exports.viewport = (el) => {
   console.log('view created')
+  return new SpaceView(el)
+}
+
+exports.space = (el) => {
+
 }
 
 exports.geom = require('./lib/geom')
