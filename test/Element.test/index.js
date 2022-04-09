1db4e9f4f010397d5241462399c7b799b07438ac
Akseli Palén <akseli.palen@gmail.com>
2022-04-10 00:25:37 +0300

    test and fix Element:transformBy

--- test/Element.test/index.js
+++ test/Element.test/index.js
@@ -1,5 +1,7 @@
 const affinedom = require('../../index')
 const template = require('./template.ejs')
+require('./zeroedge.css')
+require('../../affinedom.css')
 
 module.exports = function (test) {
   const container = document.getElementById('container')
@@ -32,5 +34,25 @@ module.exports = function (test) {
     t.equal(xyOnView.y, 6)
 
     t.end()
+  })
+
+  test('Element:transformBy', (t) => {
+    // Setting
+    container.innerHTML = template()
+    const aview = affinedom.viewport('.affine-viewport')
+    const aelem = affinedom('.affine-element')
+    const tr = affinedom.createTransform(aview, {
+      rotate: Math.PI / 6 // rotate 30 degrees clockwise
+    })
+    // Test and allow browser a moment to render
+    setTimeout(() => {
+      t.equal(document.elementFromPoint(300, 20),
+        aelem.el, 'element at 300,20')
+      aelem.transformBy(tr)
+      t.notEqual(document.elementFromPoint(300, 20),
+        aelem.el, 'element not at 300,20')
+
+      t.end()
+    }, 0)
   })
 }
