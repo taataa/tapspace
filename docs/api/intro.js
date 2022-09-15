f63f2947c86c20bddeeebe2b0854235ffbd2a703
Akseli Palén <akseli.palen@gmail.com>
2022-09-15 16:40:36 +0300

    improve API docs intro

---
+++ docs/api/intro.js
@@ -0,0 +1,9 @@
+module.exports = `
+Welcome to Tapspace API documentation.
+You can build your zoomable application with the tools documented here.
+These docs are generated with [yamdog](https://github.com/axelpale/yamdog).
+
+Syntax help: \`ClassName\`, \`namespace\`, \`.CONSTANT\`, \`.classMethod()\`, \`:instanceProperty\`, \`:instanceMethod()\`, and \`[optionalParameter]\`.
+
+See also: [Introduction](https://taataa.github.io/tapspace/) – [Tutorial](https://taataa.github.io/tapspace/tutorial/) - [GitHub](https://github.com/taataa/tapspace)
+`
