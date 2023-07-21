c4ca4df20a08fdb34f82d689b68c3bad5084d45d
Akseli Palen <akseli.palen@gmail.com>
2023-07-21 15:19:00 +0300

    implement Circle:atArc

--- test/geometry.test/Circle.test/points.test.js
+++ test/geometry.test/Circle.test/points.test.js
@@ -2,6 +2,38 @@ const template = require('./template.ejs')
 
 module.exports = function (test, container, tapspace) {
   //
+  test('Circle :atArc', (t) => {
+    // Setup
+    container.innerHTML = template()
+    const view = tapspace.createView('#testspace')
+    const space = tapspace.createSpace()
+    view.addChild(space)
+
+    const plain = { x: 1, y: 1, z: 1, r: 1 }
+    const circle = new tapspace.geometry.Circle(space, plain)
+
+    t.almostEqualPoint(
+      circle.atArc(Math.PI),
+      space.at(0, 1, 1),
+      'should match point at left perimeter'
+    )
+
+    t.almostEqualPoint(
+      circle.atArc(0),
+      space.at(2, 1, 1),
+      'should match point at right perimeter'
+    )
+
+    t.almostEqualPoint(
+      circle.atArc(space.getDirection(Math.PI / 2)),
+      space.at(1, 2, 1),
+      'should handle direction tensor'
+    )
+
+    t.end()
+  })
+
+  //
   test('Circle :atCenter', (t) => {
     // Setup
     container.innerHTML = template()
@@ -15,7 +47,7 @@ module.exports = function (test, container, tapspace) {
     t.almostEqualPoint(
       circle.atCenter(),
       space.at(1, 1, 1),
-      'should match point'
+      'should match point at center'
     )
 
     t.end()
