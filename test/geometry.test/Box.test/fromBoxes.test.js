93d0395e1aa387839fc0837d4f623ee0f26160cc
Akseli Palen <akseli.palen@gmail.com>
2023-10-13 16:28:37 +0300

    improve Box:fromBoxes to allow Basis

--- test/geometry.test/Box.test/fromBoxes.test.js
+++ test/geometry.test/Box.test/fromBoxes.test.js
@@ -2,6 +2,8 @@ const template = require('./template.ejs')
 
 module.exports = function (test, container, tapspace) {
   //
+  const Box = tapspace.geometry.Box
+
   test('Box:fromBoxes', (t) => {
     // Setup
     container.innerHTML = template()
@@ -17,8 +19,8 @@ module.exports = function (test, container, tapspace) {
 
     const boxes = items.map(item => item.getBoundingBox())
 
-    // Get the box, represent on item
-    const bounds = tapspace.geometry.Box.fromBoxes(view, boxes)
+    // Get the box, represent on viewport
+    const bounds = Box.fromBoxes(view, boxes)
 
     t.deepEqual(
       bounds.at(0, 0).getRaw(),
@@ -29,6 +31,26 @@ module.exports = function (test, container, tapspace) {
       bounds.getSize().getRaw(),
       { w: 4, h: 4, d: 2 },
       'should have correct origin'
+    )
+
+    // Get bounding box on custom basis
+    const scale = 1
+    const angle = Math.PI / 4 // 45 deg
+    const basis = space.createBasis({ x: 0, y: 0 }, scale, angle)
+    const orientation = basis.getOrientation()
+
+    const orientedBoxes = items.map(item => item.getBoundingBox(orientation))
+    const orientedBounds = Box.fromBoxes(basis, orientedBoxes)
+
+    t.almostEqualPoint(
+      orientedBounds.atNorm(0, 0).getRaw(),
+      { x: 0, y: -Math.SQRT2, z: 0 },
+      'should have correct oriented origin'
+    )
+    t.deepEqual(
+      orientedBounds.atNorm(1, 1).getRaw(),
+      { x: 2, y: 2 + Math.SQRT2, z: 0 },
+      'should have correct oriented corner'
     )
 
     t.end()
