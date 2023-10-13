38fe363c17fbb179e1312f3abead18584d26eb5e
Akseli Palen <akseli.palen@gmail.com>
2023-10-13 21:14:31 +0300

    improve Composite:getBoundingBox to allow custom orientation

--- test/components.test/Space.test/getBoundingBox.test.js
+++ test/components.test/Space.test/getBoundingBox.test.js
@@ -21,13 +21,29 @@ module.exports = function (test, container, tapspace) {
     space.addChild(foo, space.at(100, 100, 10))
     space.addChild(bar, space.at(500, 300, -10))
 
-    // Take a direction
+    // Take the box
     const bounds = space.getBoundingBox()
 
     t.deepEqual(
       bounds.getSize().getRaw(),
       { w: 600, h: 400, d: 20 },
       'should have correct size'
+    )
+
+    // Take a custom orientation.
+    const orient = space.createOrientation(Math.PI / 4)
+    const orientedBounds = space.getBoundingBox(orient)
+
+    t.almostEqualPoint(
+      orientedBounds.at(0, 0).changeBasis(space),
+      space.at(300, -100, -10),
+      'should have matching origin'
+    )
+
+    t.almostEqualPoint(
+      orientedBounds.atNorm(1, 1).changeBasis(space),
+      space.at(500, 700, -10),
+      'should have matching bottom right corner'
     )
 
     t.end()
