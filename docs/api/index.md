2970e6056a6c7ee8dfb6bb4545744d1f7722f2b2
Akseli Palen <akseli.palen@gmail.com>
2023-12-15 04:55:39 +0200

    improve api v2 intro

--- docs/api/index.md
+++ docs/api/index.md
@@ -3,6 +3,6 @@
 Choose version:
 
 - [Tapspace v1.6.0 API Docs](https://taataa.github.io/tapspace/api/v1/)
-- [Tapspace v2.0.0-alpha API Docs](v2/)
+- [Tapspace v2.0.0 API Docs](v2/)
 
 Or go [back to introduction](../)
--- docs/api/v2/intro.js
+++ docs/api/v2/intro.js
@@ -1,11 +1,16 @@
 module.exports = `
-Welcome to Tapspace.js API documentation.
-Build your zoomable application with the tools documented here.
-For older versions, see [Tapspace.js API docs index](https://taataa.github.io/tapspace/api/)
+Choose version: [v1](https://taataa.github.io/tapspace/api/) – [v2](#)
 
-This document follows these naming conventions: \`ClassName\`, \`namespace\`, \`.CONSTANT\`, \`.classMethod()\`, \`:instanceProperty\`, \`:instanceMethod()\`, \`[optionalParameter]\`, and \`optionalProperty?\`.
+**Welcome** to Tapspace.js API documentation.
+Build your zoomable application with the classes and methods documented here.
+This document is generated with [yamdog](https://github.com/axelpale/yamdog).
+
+You might want to look [examples](https://taataa.github.io/tapspace/#examples)
+and [tutorial](https://taataa.github.io/tapspace/tutorial/) to learn how to combine
+classes and method calls to build your first zoomable applications. Rely on the API documentation
+when you are ready to get wild.
+
+**Naming convention** of the document: \`ClassName\`, \`namespace\`, \`.CONSTANT\`, \`.classMethod()\`, \`:instanceProperty\`, \`:instanceMethod()\`, \`[optionalParameter]\`, and \`optionalProperty?\`.
 
 See also: [Introduction](https://taataa.github.io/tapspace/) – [Examples](https://taataa.github.io/tapspace/#examples) – [Tutorial](https://taataa.github.io/tapspace/tutorial/) - [Glossary](https://taataa.github.io/tapspace/glossary/v2/) - [GitHub](https://github.com/taataa/tapspace)
-
-This document is generated with [yamdog](https://github.com/axelpale/yamdog).
 `
