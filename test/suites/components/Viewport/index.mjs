bee80d9098313e1e18e77af7be0067b72bf4faf7
Akseli Palen <akseli.palen@gmail.com>
2025-01-03 18:02:47 +0200

    migrate Viewport:measureGroup tests

--- test/suites/components/Viewport/index.mjs
+++ test/suites/components/Viewport/index.mjs
@@ -1,5 +1,7 @@
 import { run as runGetAspectRatio } from './getAspectRatio.test.mjs'
+import { run as runMeasureGroup } from './measureGroup.test.mjs'
 
 export const run = (test, browser) => {
   runGetAspectRatio(test, browser)
+  runMeasureGroup(test, browser)
 }
---
+++ test/suites/components/Viewport/measureGroup.test.mjs
@@ -0,0 +1,27 @@
+import { join } from 'path'
+
+const pageUrl = 'file:' + join(import.meta.dirname, 'measureGroup.test.html')
+
+export const run = (test, browser) => {
+  test('Viewport:measureGroup', async (t) => {
+    // Setup
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1000, height: 500 })
+    await page.goto(pageUrl, { waitUntil: 'domcontentloaded' })
+
+    // Test
+    const suite = await page.evaluate(() => {
+      return window.suite
+    })
+
+    for (let i = 0; i < suite.length; i += 1) {
+      const predicate = suite[i][0]
+      const message = suite[i][1]
+      t.ok(predicate, message)
+    }
+
+    // Exit
+    await page.close()
+    t.end()
+  })
+}
