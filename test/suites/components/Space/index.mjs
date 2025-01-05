d47ef2226a9f5979efdad4856e230d6132338b3a
Akseli Palen <akseli.palen@gmail.com>
2025-01-05 15:40:25 +0200

    migrate Space test suite

---
+++ test/suites/components/Space/index.mjs
@@ -0,0 +1,47 @@
+import { join } from 'path'
+
+const getUrl = (testName) => 'file:' + join(import.meta.dirname, testName)
+
+export default function (test, browser) {
+  test('Space:at', async (t) => {
+    // Setup
+    const pageUrl = getUrl('at.test.html')
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1000, height: 500 })
+    await page.goto(pageUrl, { waitUntil: 'domcontentloaded' })
+
+    // Test
+    const suite = await page.evaluate(() => window.suite)
+
+    for (let i = 0; i < suite.length; i += 1) {
+      const predicate = suite[i][0]
+      const message = suite[i][1]
+      t.ok(predicate, message)
+    }
+
+    // Exit
+    await page.close()
+    t.end()
+  })
+
+  test('Space:getBoundingBox', async (t) => {
+    // Setup
+    const pageUrl = getUrl('getBoundingBox.test.html')
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1000, height: 500 })
+    await page.goto(pageUrl, { waitUntil: 'domcontentloaded' })
+
+    // Test
+    const suite = await page.evaluate(() => window.suite)
+
+    for (let i = 0; i < suite.length; i += 1) {
+      const predicate = suite[i][0]
+      const message = suite[i][1]
+      t.ok(predicate, message)
+    }
+
+    // Exit
+    await page.close()
+    t.end()
+  })
+}
--- test/suites/components/index.mjs
+++ test/suites/components/index.mjs
@@ -1,5 +1,7 @@
+import testSpace from './Space/index.mjs'
 import testViewport from './Viewport/index.mjs'
 
 export default function (test, browser) {
+  testSpace(test, browser)
   testViewport(test, browser)
 }
