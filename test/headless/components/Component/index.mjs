eae420aa40b4bd694e615c341fd96fd0e6045ad8
Akseli Palen <akseli.palen@gmail.com>
2025-01-07 02:51:06 +0200

    migrate Component tests: prependChild, removeChild, replaceChild, replaceParent

--- test/headless/components/Component/index.mjs
+++ test/headless/components/Component/index.mjs
@@ -1,4 +1,8 @@
 export default function (test) {
   test('Component: findCommonAncestor', import.meta.dirname, 'findCommonAncestor.html')
   test('Component: geometry creation', import.meta.dirname, 'geometryCreation.html')
+  test('Component: prependChild', import.meta.dirname, 'prependChild.html')
+  test('Component: removeChild', import.meta.dirname, 'removeChild.html')
+  test('Component: replaceChild', import.meta.dirname, 'replaceChild.html')
+  test('Component: replaceParent', import.meta.dirname, 'replaceParent.html')
 }
