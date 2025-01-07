5254314da7ccea557ca0169c0b8be300c0b7a715
Akseli Palen <akseli.palen@gmail.com>
2025-01-07 02:53:15 +0200

    remove migrated original Component tests

--- test/components.test/Component.test/index.js
+++ test/components.test/Component.test/index.js
@@ -1,8 +1,4 @@
 const units = {
-  prependChild: require('./prependChild.test'),
-  removeChild: require('./removeChild.test'),
-  replaceChild: require('./replaceChild.test'),
-  replaceParent: require('./replaceParent.test'),
   setId: require('./setId.test'),
   setParent: require('./setParent.test')
 }
