f49f83d77a47c3798c610d061e4ac201dcb22c81
Akseli Palen <akseli.palen@gmail.com>
2025-01-16 16:56:20 +0200

    test Sphere :almostEqual :equal

--- test/headless/geometry/Sphere/index.mjs
+++ test/headless/geometry/Sphere/index.mjs
@@ -5,7 +5,7 @@ export default function (test) {
     'collisions',
     'construction',
     'dimensions',
-    // 'equality',
+    'equality',
     'measures',
     'points',
     'transformations',
