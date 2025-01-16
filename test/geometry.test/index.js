2faf5ca20fb4afc1ed3bbe6b8c2506d604a33d1e
Akseli Palen <akseli.palen@gmail.com>
2025-01-16 17:02:34 +0200

    remove migrated original geometry test suites

--- test/geometry.test/index.js
+++ test/geometry.test/index.js
@@ -1,9 +1,4 @@
 const units = {
-  Area: require('./Area.test'),
-  Basis: require('./Basis.test'),
-  Box: require('./Box.test'),
-  Circle: require('./Circle.test'),
-  Direction: require('./Direction.test'),
   Distance: require('./Distance.test'),
   Orientation: require('./Orientation.test'),
   Path: require('./Path.test'),
@@ -11,7 +6,6 @@ const units = {
   Ray: require('./Ray.test'),
   Scale: require('./Scale.test'),
   Size: require('./Size.test'),
-  Sphere: require('./Sphere.test'),
   Transform: require('./Transform.test'),
   Vector: require('./Vector.test')
 }
