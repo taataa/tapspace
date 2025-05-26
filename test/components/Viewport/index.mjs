7df0d8a0d0c4738fa95295c059e1132b9ed6def6
Akseli Palen <akseli.palen@gmail.com>
2025-05-26 15:29:58 +0300

    implement Viewport:hasControl

--- test/components/Viewport/index.mjs
+++ test/components/Viewport/index.mjs
@@ -1,4 +1,5 @@
 export default function (test) {
   test('Viewport:getAspectRatio', import.meta.dirname, 'getAspectRatio.html')
+  test('Viewport:hasControl', import.meta.dirname, 'hasControl.html')
   test('Viewport:measureGroup', import.meta.dirname, 'measureGroup.html')
 }
